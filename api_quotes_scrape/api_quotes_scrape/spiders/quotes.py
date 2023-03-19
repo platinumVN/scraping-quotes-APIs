2b22233149488195b75fe62cafc56560d02770da
thupla <thuleanhphan@gmail.com>
2023-03-19 16:00:01 +0700

    pagination

--- api_quotes_scrape/api_quotes_scrape/spiders/quotes.py
+++ api_quotes_scrape/api_quotes_scrape/spiders/quotes.py
@@ -10,7 +10,18 @@ class QuotesSpider(scrapy.Spider):
     def parse(self, response):
         response_json = response.json()
         current_page = response_json.get("page")
-        quotes = response_json.get(quotes)
+        quotes = response_json.get("quotes")
+        has_next_page = response_json.get('has_next')
         yield {
-            'current_page' : current_page
+            'current_page' : current_page,
+            'quotes_data' : quotes 
         }
+
+        # if next page exists:
+        if has_next_page:
+            next_page_url = f"https://quotes.toscrape.com/api/quotes?page={int(current_page) + 1}"
+
+            yield scrapy.Request(
+                url=next_page_url,
+                callback=self.parse
+            )
