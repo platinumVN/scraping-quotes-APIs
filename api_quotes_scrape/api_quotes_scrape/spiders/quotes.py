8f5ab5946bbce139bfa94f9ebd6a64ae52023eae
thupla <thuleanhphan@gmail.com>
2023-03-19 15:54:34 +0700

    one page

--- api_quotes_scrape/api_quotes_scrape/spiders/quotes.py
+++ api_quotes_scrape/api_quotes_scrape/spiders/quotes.py
@@ -1,10 +1,16 @@
 import scrapy
-
+import json
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
     allowed_domains = ["quotes.toscrape.com"]
-    start_urls = ["http://quotes.toscrape.com/"]
+    # start_urls = ["https://quotes.toscrape.com/scroll"]
+    start_urls = ["https://quotes.toscrape.com/api/quotes?page=1"]
 
     def parse(self, response):
-        pass
+        response_json = response.json()
+        current_page = response_json.get("page")
+        quotes = response_json.get(quotes)
+        yield {
+            'current_page' : current_page
+        }
